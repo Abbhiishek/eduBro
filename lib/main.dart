d512c35484d4cd4930553a545b7c6ef4ad29f49a
Abhishek Kushwaha <abhishekkushwaha1479@gmail.com>
2023-04-21 23:16:46 +0530

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,23 +10,36 @@ import 'package:sensei/models/user_model.dart';
 import 'package:sensei/router.dart';
 import 'package:sensei/theme/pallete.dart';
 import 'package:sensei/firebase_options.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  // FirebaseMessaging.instance.requestPermission();
-  // FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-  //   // Show a popup with the message details
-  //   print(message);
-  // });
+
+  AwesomeNotifications().initialize(
+      // set the icon to null if you want to use the default app icon
+      null,
+      [
+        NotificationChannel(
+            channelKey: 'sensei',
+            channelName: 'Sensei Channel Delivery Service',
+            channelDescription: 'Made with Love by Sensei',
+            defaultColor: const Color(0xFF9D50DD),
+            ledColor: Colors.white)
+      ],
+      debug: true);
+
   runApp(const ProviderScope(child: MyApp()));
   // debugDumpApp();
 }
 
 class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
+
+  static final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey<NavigatorState>();
 
   static BuildContext? context;
 
@@ -50,11 +62,36 @@ class _MyAppState extends ConsumerState<MyApp> {
   }
 
   @override
+  void initState() {
+    // Only after at least the action method is set, the notification events are delivered
+    // AwesomeNotifications().setListeners(
+    //     onActionReceivedMethod: NotificationController.onActionReceivedMethod,
+    //     onNotificationCreatedMethod:
+    //         NotificationController.onNotificationCreatedMethod,
+    //     onNotificationDisplayedMethod:
+    //         NotificationController.onNotificationDisplayedMethod,
+    //     onDismissActionReceivedMethod:
+    //         NotificationController.onDismissActionReceivedMethod);
+
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        // This is just a basic example. For real apps, you must show some
+        // friendly dialog box before call the request method.
+        // This is very important to not harm the user experience
+        AwesomeNotifications().requestPermissionToSendNotifications();
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    MyApp.context = context; // Set the context variable in the build method
+    // MyApp.context = context; // Set the context variable in the build method
     return ref.watch(authStateChangeProvider).when(
           data: (data) => MaterialApp.router(
             title: 'Sensei',
+            key: MyApp.navigatorKey,
             debugShowCheckedModeBanner: false,
             theme: ref.watch(themeNotifierProvider),
             routerDelegate: RoutemasterDelegate(routesBuilder: (context) {
