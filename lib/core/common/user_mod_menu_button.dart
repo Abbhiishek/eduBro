ba5b4a8ce1db620396adba8f43045834d83807fb
Abhishek Kushwaha <abhishekkushwaha1479@gmail.com>
2023-04-22 22:40:08 +0530

    update user_mod_menu_button.dart

--- lib/core/common/user_mod_menu_button.dart
+++ lib/core/common/user_mod_menu_button.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:routemaster/routemaster.dart';
-import 'package:sensei/features/auth/controller/auth_controller.dart';
 import 'package:sensei/features/post/controller/post_controller.dart';
 import 'package:sensei/models/post_model.dart';
 import 'package:sensei/models/user_model.dart';
